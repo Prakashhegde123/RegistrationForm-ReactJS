71a6c17743eecf1d22ee1699d177c1f4e5ceecaa
hegde <hegdeprakash6@gmail.com>
2020-08-07 15:05:07 +0530

    task one completed

---
+++ src/User.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const User = (props) => {
+ 
+   
+    return(
+        <div >  
+        <h3 style={{color:"black"}}> {props.name}</h3>
+        <h3 style={{color:"black"}} > {props.email}</h3>
+        </div>
+    );
+}
+
+
+
+export default User;
